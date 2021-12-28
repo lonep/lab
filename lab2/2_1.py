7704184bd300a6ed2361549eca8168dcddc28de5
lonep <djoefrit@gmail.com>
2021-12-28 13:03:11 +0300

    Update 2_1.py

--- lab2/2_1.py
+++ lab2/2_1.py
@@ -1,4 +1,4 @@
-from re import X
+﻿from re import X
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,29 +8,36 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
 
-Davis = pd.read_csv('Davis.csv')
+df = pd.read_csv('Davis.csv')
 
-Davis.dropna(inplace=True);
-Davis = Davis.drop(Davis[Davis.height < 100].index )
-Davis = Davis.drop(Davis[Davis.height > 215].index )
 
-Davis = Davis.drop(Davis[Davis.weight < 40].index )
-Davis = Davis.drop(Davis[Davis.weight > 140].index )
-#print(Davis)
+df.drop('Unnamed: 0', axis=1, inplace=True)
 
-train, test = train_test_split(Davis, test_size = 50)
+#Убрали аномальные значения из выборки
+df = df.drop(df[df.height < 100].index )
+df = df.drop(df[df.height > 215].index )
+
+df = df.drop(df[df.weight < 40].index )
+df = df.drop(df[df.weight > 140].index )
+
+print(df.head())
+print(df.isna().sum())
+
+
+train, test = train_test_split(df, test_size = 50)
 
 
 fig, axes = plt.subplots(3,2)
 
-axes[0][0].hist(train['height'], bins='auto', color = 'orange', label = 'height')
-axes[0][1].hist(train['weight'], bins='auto', label = 'weight')
+axes[0][0].hist(df['height'], bins='auto', color = 'orange', label = 'height')
+axes[0][1].hist(df['weight'], bins='auto', label = 'weight')
 
-axes[1][0].hist(train[train['sex'] == 'M']['height'], bins='auto', label = 'Man height')
-axes[1][1].hist(train[train['sex'] == 'M']['weight'], bins='auto', label = 'Man weight')
+axes[1][0].hist(df[df['sex'] == 'M']['height'], bins='auto', label = 'Man height')
+axes[1][1].hist(df[df['sex'] == 'M']['weight'], bins='auto', label = 'Man weight')
 
-axes[2][0].hist(train[train['sex'] == 'F']['height'], bins='auto', color = 'pink', label = 'Female height')
-axes[2][1].hist(train[train['sex'] == 'F']['weight'], bins='auto', color = 'pink', label = 'Female weight')
+axes[2][0].hist(df[df['sex'] == 'F']['height'], bins='auto', color = 'pink', label = 'Female height')
+axes[2][1].hist(df[df['sex'] == 'F']['weight'], bins='auto', color = 'pink', label = 'Female weight')
+
 
 for i in range(3):
     for j in range(2):
@@ -38,6 +45,9 @@ for i in range(3):
 
 train.replace({'M':0,'F':1}, inplace=True)
 test.replace({'M':0,'F':1}, inplace=True)
+
+print(test.head())
+print(train.head())
 
 trainX = train.loc[:,'weight':'height'].to_numpy()
 trainY = train['sex'].to_numpy()
@@ -54,18 +64,45 @@ prd = clf.predict(trainX)
 number = 2
 fig1, axes1 = plt.subplots(number)
 
-for i in range(number):
-    x1_min, x1_max = trainX[:,0].min() - 0.5, trainX[:,1].max()+0.5
-    x2_min, x2_max = trainX[:,0].min() - 0.5, trainX[:,1].max()+0.5
-    xx1,xx2 = np.mgrid[x1_min:x1_max:100j, x2_min:x2_max:100j]
-    X_pred = np.column_stack([xx1.reshape(-1), xx2.reshape(-1)])
-    y_pred = clf.predict(X_pred)
-    axes1[i].scatter(trainX[prd == 0][:,1], trainX[prd == 0][:,0], color='orange', label='Man')
-    axes1[i].scatter(trainX[prd == 1][:,1], trainX[prd == 1][:,0], color='blue', label='Female')
-    axes1[i].pcolormesh(xx1, xx2, y_pred.reshape(xx1.shape), cmap=ListedColormap(['orange','blue']), alpha=0.45, shading='auto')
-    axes1[i].set_ylabel('weight')
-    axes1[i].set_xlabel('height')
-    axes1[i].legend()
+axes1[0].set_title('Тренировочная выборка')
+axes1[1].set_title('Тестовая выборка')
+
+x1_min, x1_max = trainX[:,0].min() - 0.5, trainX[:,0].max()+0.5
+x2_min, x2_max = trainX[:,1].min() - 0.5, trainX[:,1].max()+0.5
+
+meshgrid = 0.5
+
+xx1,xx2 = np.meshgrid(np.arange(x1_min, x1_max, h), np.arange(x2_min, x2_max, h))
+Z = clf.predict(np.c_[xx1.ravel(), xx2.ravel()])
+
+
+Z = Z.reshape(xx1.shape)
+axes1[0].pcolormesh(xx1, xx2, Z, cmap=ListedColormap(['orange','blue']),shading="auto", alpha=0.3)
+
+axes1[0].scatter(trainX[prd == 0][:,0], trainX[prd == 0][:,1], color='orange', label='M')
+axes1[0].scatter(trainX[prd == 1][:,0], trainX[prd == 1][:,1], color='blue', label='F')
+axes1[0].set_ylabel('weight')
+axes1[0].set_xlabel('height')
+axes1[0].legend()
+
+x1_min, x1_max = testX[:,0].min() - 0.5, testX[:,0].max()+0.5
+x2_min, x2_max = testX[:,1].min() - 0.5, testX[:,1].max()+0.5
+
+xx1,xx2 = np.meshgrid(np.arange(x1_min, x1_max,h), np.arange(x2_min, x2_max, h))
+Z = clf.predict(np.c_[xx1.ravel(), xx2.ravel()])
+
+Z = Z.reshape(xx1.shape)
+pred = clf.predict(testX)
+
+axes1[1].pcolormesh(xx1, xx2, Z, cmap=ListedColormap(['orange','blue']),shading="auto", alpha=0.3)
+axes1[1].scatter(testX[pred == 0][:,0], testX[pred == 0][:,1], color='orange', label='M')
+axes1[1].scatter(testX[pred == 1][:,0], testX[pred == 1][:,1], color='blue', label='F')
+axes1[1].set_ylabel('weight')
+axes1[1].set_xlabel('height')
+axes1[1].legend()
+
+
+
     
 plt.show()
 
